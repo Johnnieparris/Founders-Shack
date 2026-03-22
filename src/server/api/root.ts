2f000d819d284b672b308060e5021ac994773efa
Your Name <tarunsathish1001@gmail.com>
2026-03-22 11:18:30 +1100

    scrapper for rubric compleated and connected to db

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,6 @@
+import { eventRouter } from "~/server/api/routers/event";
 import { postRouter } from "~/server/api/routers/post";
+import { societyRouter } from "~/server/api/routers/society";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 
 /**
@@ -8,6 +10,8 @@ import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
  */
 export const appRouter = createTRPCRouter({
   post: postRouter,
+  event: eventRouter,
+  society: societyRouter,
 });
 
 // export type definition of API
